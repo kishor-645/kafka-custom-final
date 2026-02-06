419b5fe681fc28fcb8191e13f4823c744b168421
kishor <kishor>
2026-02-06 17:56:15 +0530

    single node working

--- deploy-kafka.sh
+++ deploy-kafka.sh
@@ -4,7 +4,7 @@
 ENV_PREFIX=${1:-"k"}
 NAMESPACE="${ENV_PREFIX}"
 # Make sure the Release Name is short to avoid label truncation issues
-RELEASE_NAME="${ENV_PREFIX}-kfk"
+RELEASE_NAME="${ENV_PREFIX}-stackforge-kfk"
 CHART_PATH="." # Path to your local folder
 
 # Updated HA Strategy for this specific chart
@@ -33,11 +33,11 @@ helm upgrade --install "$RELEASE_NAME" "$CHART_PATH" \
   --set kafka.broker.replicaCount=1 \
   --set kafka.broker.resources.requests.memory="1Gi" \
   --set kafka.broker.resources.limits.memory="2Gi" \
-  --set kafka.broker.service.loadBalancerIP="10.227.252.62" \
+  --set kafka.broker.service.loadBalancerIP="10.227.252.60" \
   --set kafka.broker.listeners.internalName="INTERNAL" \
   --set kafka.broker.listeners.externalName="EXTERNAL" \
   --set kafka.broker.listeners.internalPort=9092 \
   --set kafka.broker.listeners.externalPort=9094 \
-  --set kafka.broker.externalAdvertisedHost="10.227.252.62" \
+  --set kafka.broker.externalAdvertisedHost="10.227.252.60" \
   --set kafkaUI.enabled=true
     
